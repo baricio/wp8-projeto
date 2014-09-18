2fb09f745725892b1ab5ee259026742dd150327b
baricio <baricio@gmail.com>
2014-09-18 11:35:51 -0300

    Navegacao de cadastro pra login

--- LocalizaImoveisApp/MainPage.xaml.cs
+++ LocalizaImoveisApp/MainPage.xaml.cs
@@ -22,6 +22,11 @@ namespace LocalizaImoveisApp
             //BuildLocalizedApplicationBar();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Login.xaml", UriKind.Relative));
+        }
+
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
         //{
