775df24c924bb2397195ca12144008a5cbda1e5e
baricio <baricio@gmail.com>
2014-09-19 21:05:41 -0300

    teste

---
+++ LocalizaImoveisApp/Mapa.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Maps.Controls;
+
+namespace LocalizaImoveisApp
+{
+    public partial class Mapa : PhoneApplicationPage
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+            Map mapa1 = new Map();
+            ContentPanel.Children.Add(mapa1);
+        }
+
+        public void getMap(){
+
+            Map mapa1 = new Map();
+            ContentPanel.Children.Add(mapa1);
+        }
+    }
+}
